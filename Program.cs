88702079ba774171cac2c6e9a647f9242d64f797
xalmanfaris <xalmanfarisp@gmail.com>
2025-12-23 16:58:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,39 +4,19 @@ class Program
 {
   static void Main()
     {
-       int mark = 90;
+       int a = 10, b = 25, c = 15;
 
-if (mark >= 90 && mark <= 100)
+if (a >= b && a >= c)
 {
-    Console.WriteLine("S grade");
+    Console.WriteLine("Largest: " + a);
 }
-else if (mark >= 80 && mark < 90)
+else if (b >= a && b >= c)
 {
-    Console.WriteLine("A grade");
-}
-else if (mark >= 70 && mark < 80)
-{
-    Console.WriteLine("B grade");
-}
-else if (mark >= 60 && mark < 70)
-{
-    Console.WriteLine("C grade");
-}
-else if (mark >= 50 && mark < 60)
-{
-    Console.WriteLine("D grade");
-}
-else if (mark >= 40 && mark < 50)
-{
-    Console.WriteLine("E grade");
-}
-else if (mark >= 0 && mark < 40)
-{
-    Console.WriteLine("Student has failed");
+    Console.WriteLine("Largest: " + b);
 }
 else
 {
-    Console.WriteLine("Invalid marks");
+    Console.WriteLine("Largest: " + c);
 }
 
     }
