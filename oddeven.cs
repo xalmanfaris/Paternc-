b0292da2bdbe8f66bd99baf0f303fb1b3188efe8
xalmanfaris <xalmanfarisp@gmail.com>
2025-12-23 17:03:11 +0530

    Add files via upload

---
+++ oddeven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+    {
+    int num = 7;
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
+
+
+    }
+}
