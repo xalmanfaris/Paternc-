67ba7faff001bf465ffc46a9c1161bf23eb7da7a
xalmanfaris <xalmanfarisp@gmail.com>
2025-12-23 17:01:55 +0530

    Add files via upload

---
+++ leapyear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+    {
+        int year = 2024;
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Not a leap year");
+}
+
+    }
+}
