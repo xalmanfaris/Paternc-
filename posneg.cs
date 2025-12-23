9db51542a87eff80f2ae2501b4a2badeeb943a59
xalmanfaris <xalmanfarisp@gmail.com>
2025-12-23 17:04:27 +0530

    Add files via upload

---
+++ posneg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+    {
+   int num = -5;
+
+if (num > 0)
+{
+    Console.WriteLine("Positive");
+}
+else if (num < 0)
+{
+    Console.WriteLine("Negative");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
+
+
+
+    }
+}
